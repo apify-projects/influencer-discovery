b58064e4c6e600f108fffd4108d1ec6af87fc77c
Sviat <svpetrenko123@gmail.com>
2025-05-28 15:13:40 +0300

    section

--- src/user-search.ts
+++ src/user-search.ts
@@ -14,6 +14,7 @@ export function performTikTokUserSearch() {
                 searchQueries: state.searchTermsToScrape,
                 resultsPerPage: 20,
                 maxProfilesPerQuery: 100,
+                searchSection: '/user',
             },
         );
 
