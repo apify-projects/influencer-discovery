d6250f0ad366964be182e250e1c34f0853fdb1c6
Sviat <svpetrenko123@gmail.com>
2025-05-28 13:03:04 +0300

    schema

--- src/evaluate-profile.ts
+++ src/evaluate-profile.ts
@@ -1,7 +1,20 @@
 import { ChatOpenAI } from '@langchain/openai';
+import { z } from 'zod';
 import { Dataset } from 'apify';
 import { State, StateAnnotation } from './state.js';
 import { SITE_NAME_FOR_LLM } from './consts.js';
+
+const FIT_SCALE = {
+    0.2: 'not a fit at all',
+    0.4: 'a poor fit',
+    0.6: 'mediocre fit',
+    0.8: 'a good fit',
+    0.9: 'a great fit',
+    1.0: 'a perfect fit',
+};
+const fitScaleMessage = Object.entries(FIT_SCALE)
+    .map(([key, value], i, arr) => `from ${i > 0 ? arr[i - 1][0] : 0} to ${key} - ${value}`)
+    .join(', ');
 
 export const evaluateProfiles = (model: ChatOpenAI) => async (state: State): Promise<typeof StateAnnotation.Update> => {
     const { profilesToEvaluate, influencerDescription, scrapedProfiles } = state;
@@ -11,27 +24,31 @@ export const evaluateProfiles = (model: ChatOpenAI) => async (state: State): Pro
         return profile;
     })
         .filter((profile) => profile !== undefined);
-    const result = await model.invoke([
-        {
-            role: 'system',
-            content: `You are a helpful assistant that determines whether a ${SITE_NAME_FOR_LLM} profile`
-                + ` is a good influencer according to the given description.`,
-        },
-        {
-            role: 'user',
-            // TODO: do not stringify
-            content: `Profile info: ${JSON.stringify(scrapedProfilesToEvaluate)}`
-                // TODO: provide output JSON format
-                + `Task: determine what profiles are influencer that fit the following description well: `
-                + `"${influencerDescription}"`,
-        },
-    ]);
-    // TODO: once we have output schema, fix this
-    const parsedResult = JSON.parse(result.content.toString()) as {
-        goodFitProfiles: string[];
-    };
-    // TODO: correct output format
-    await Dataset.pushData(parsedResult.goodFitProfiles);
+    const result = await model
+        .withStructuredOutput(z.array(
+            z.object({
+                // TODO: specify under what key/where it is provided
+                profile: z.string().describe('The profile handle, as provided in the input'),
+                fit: z.number().describe(`How well the profile fits the influencer description: ${fitScaleMessage}.`),
+                fitDescription: z.string().describe('Why such a fit score was given.'),
+            }),
+        ))
+        .invoke([
+            {
+                role: 'system',
+                content: `You are a helpful assistant that determines whether a ${SITE_NAME_FOR_LLM} profile`
+                    + ` is a good influencer according to the given description.`,
+            },
+            {
+                role: 'user',
+                // TODO: do not stringify
+                content: `Profile info: ${JSON.stringify(scrapedProfilesToEvaluate)}`
+                    // TODO: provide output JSON format
+                    + `Task: assign a score to each profile determining how well it fits the following influencer description: `
+                    + `"${influencerDescription}"`,
+            },
+        ]);
+    await Dataset.pushData(result);
     return {
         profilesToEvaluate: {
             // TODO: use correct mapping
