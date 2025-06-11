3f1d4b9f2e8d5825f4c5e294e90f0647bfb91e55
Luigi Ruocco <luigi.ruocco@apify.com>
2025-06-11 14:46:13 +0200

    fix: minor changes

--- src/chargingManager.ts
+++ src/chargingManager.ts
@@ -5,6 +5,5 @@ await Actor.init();
 export const chargingManager = Actor.getChargingManager();
 
 export const CHARGE_EVENT_NAMES = {
-    ACTOR_START: 'actor-start',
     PROFILE_OUTPUT: 'profile',
 };
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import { evaluateProfiles } from './evaluate-profile.js';
 import { GET_TIKTOK_PROFILE_NODE_NAME, getTikTokProfile } from './tools.js';
 import { performTikTokUserSearch, TIKTOK_USER_SEARCH_NODE_NAME } from './user-search.js';
 import { ASK_LLM_FOR_QUERIES_NODE_NAME, askLlmForQueries } from './ask-llm-for-queries.js';
-import { CHARGE_EVENT_NAMES, chargingManager } from './chargingManager.js';
 // this is ESM project, and as such, it requires you to specify extensions in your relative imports
 // read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
 // note that we need to use `.js` even when inside TS files
@@ -22,7 +21,6 @@ interface Input {
 
 // The init() call configures the Actor for its environment. It's recommended to start every Actor with an init()
 await Actor.init();
-await chargingManager.charge({ eventName: CHARGE_EVENT_NAMES.ACTOR_START, count: 1 });
 // Structure of input is defined in input_schema.json
 const {
     influencerDescription,
