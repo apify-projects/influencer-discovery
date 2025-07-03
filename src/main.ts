c943e025ae598e18d142e9b310d631f52853dfee
Luigi Ruocco <luigi.ruocco@apify.com>
2025-07-03 14:33:57 +0200

    refactor: parse input

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,22 @@
-// Apify SDK - toolkit for building Apify Actors (Read more at https://docs.apify.com/sdk/js/)
 import { StateGraph } from '@langchain/langgraph';
 import { Actor, log } from 'apify';
-// Crawlee - web scraping and browser automation library (Read more at https://crawlee.dev)
 import { ChatOpenAI } from '@langchain/openai';
 import { StateAnnotation } from './state.js';
 import { evaluateProfiles } from './evaluate-profile.js';
 import { GET_TIKTOK_PROFILE_NODE_NAME, getTikTokProfile } from './tools.js';
 import { performTikTokUserSearch, TIKTOK_USER_SEARCH_NODE_NAME } from './user-search.js';
 import { ASK_LLM_FOR_QUERIES_NODE_NAME, askLlmForQueries } from './ask-llm-for-queries.js';
-// this is ESM project, and as such, it requires you to specify extensions in your relative imports
-// read more about this here: https://nodejs.org/docs/latest-v18.x/api/esm.html#mandatory-file-extensions
-// note that we need to use `.js` even when inside TS files
-// import { router } from './routes.js';
+import type { Input } from './types.js';
 
-interface Input {
-    influencerDescription: string;
-    usernames: string[];
-    mock?: boolean;
-    generatedKeywords: number;
-    profilesPerKeyword: number;
-}
-
-// The init() call configures the Actor for its environment. It's recommended to start every Actor with an init()
 await Actor.init();
-// Structure of input is defined in input_schema.json
+
 const {
     influencerDescription,
     usernames = [],
     mock = false,
     generatedKeywords = 5,
     profilesPerKeyword = 10,
-} = await Actor.getInput<Input>() ?? {} as Input;
+} = await getInput();
 
 if (usernames.length > 0) {
     log.info(`Using the provided username. No other influencer will be searched on TikTok.`);
@@ -78,3 +64,11 @@ await chain.invoke({
 
 // Gracefully exit the Actor process. It's recommended to quit all Actors with an exit()
 await Actor.exit();
+
+async function getInput(): Promise<Input> {
+    const input = await Actor.getInput<Input>();
+    if (!input) {
+        throw new Error('No input provided');
+    }
+    return input;
+}
